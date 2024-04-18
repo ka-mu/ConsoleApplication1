6160e91580e1a86c01c5ad2c9af3f79aff330c29
Arturchik2515 <shumihinartur@gmail.com>
2024-04-18 20:32:25 +0700

    Initial commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int numOfStrings(vector<string> &patterns, string word) {
+    int a;
+    for (size_t i = 0; i < patterns.size(); i++) {
+        for (int j = 0; i < word.length(); i++) {
+            if (word[j] == patterns.at)
+        }
+    }
+}
+
+int main()
+{
+    vector<string> patterns;
+    string word;
+
+    numOfStrings(patterns, word);
+    return 0;
+}
